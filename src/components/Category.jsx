c9c7318788dc9a050d106766bfe043a0fe6e04f9
Nabin90 <nabinpunis90@gmail.com>
2022-03-24 16:09:21 +0545

    designing navlinks and their pages

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,22 +7,22 @@ import { NavLink } from 'react-router-dom';
 function Category() {
   return (
     <List>
-        <NavLink to={'/cuisine/Italian'}>
+        <SLink to={'/cuisine/Italian'}>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
+        </SLink>
+        <SLink to={'/cuisine/American'}>
             <FaHamburger/>
             <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Thai'}>
+        </SLink>
+        <SLink to={'/cuisine/Thai'}>
             <GiNoodles/>
             <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Korean'}>
+        </SLink>
+        <SLink to={'/cuisine/Korean'}>
             <GiChopsticks/>
             <h4>Korean</h4>
-        </NavLink>
+        </SLink>
     </List>
   );
 }
@@ -31,6 +31,39 @@ const List = styled.div`
     display: flex;
     justify-content: center;
     margin: 1rem 0rem;    
+`;
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 1rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 5rem;
+    height: 5rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4 {
+        color: #fff;
+        font-size: 0.8rem;
+    }    
+    svg {
+        color: #fff;
+        font-size: 1rem;
+    }
+    &.active{
+        background: linear-gradient(to right, #f27121, #e94057);
+
+        svg{
+            color: #fff;
+        }
+        h4 {
+            color: #fff;
+        }
+    }
 `;
 
 export default Category
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -19,11 +19,36 @@ function Cuisine() {
         getCuisine(params.type);
     }, [params.type]);
 
-  return (
-    <div>
-        
-    </div>
-  )
+  return <Grid>
+    {cuisine.map((item) => {
+      return (
+        <Card key={item.id}>
+          <img src= {item.image} alt="" />
+          <h4>{item.title}</h4>
+        </Card>
+      )
+    })}
+  </Grid>
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  grid-gap: 3rem;
+`; 
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 1rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 0.5rem;
+  }
+`;
 
 export default Cuisine
