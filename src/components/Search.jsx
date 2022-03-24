989dacd09b4463ff218f9a2f49dd948409bd456d
Nabin90 <nabinpunis90@gmail.com>
2022-03-24 16:50:58 +0545

    full scale searching integrated

---
+++ src/components/Search.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+function Search() {
+
+    const [input, setInput] = useState("");
+    const navigate = useNavigate();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate('/searched/' + input);
+    };
+
+  return (
+    <FormStyle onSubmit={submitHandler}>
+        <div>
+            <FaSearch></FaSearch>
+            <input onChange={(e) => setInput(e.target.value)} type="text" value={input} />
+        </div>
+    </FormStyle>
+  )
+}
+
+const FormStyle = styled.form`
+    margin: 4rem 15rem;
+    div {
+        position: relative;
+        width: 100%;
+    }
+    
+    input {
+        border: none;
+        background: linear-gradient(35deg, #494949, #313131);
+        font-size: 1rem;
+        color: #fff;
+        padding: 0.5rem 2.5rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 100%;
+    }
+    svg {
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        color: #fff;
+    }
+`;
+
+export default Search
